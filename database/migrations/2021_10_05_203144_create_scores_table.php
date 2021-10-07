4e6cfe73ba2918dc965b00416cc3afe188f5eadd
Chirag Jadhwani <chirag@chiragjadhwani.com>
2021-10-06 22:04:35 -0700

    first

---
+++ database/migrations/2021_10_05_203144_create_scores_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateScoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('scores', function (Blueprint $table) {
+            $table->id();
+            $table->Integer('response1');
+            $table->Integer('response2');
+            $table->Integer('response3');
+            $table->Integer('response4');
+            $table->Integer('response5');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('scores');
+    }
+}
