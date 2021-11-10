3ddf5a74943489267bd68a481537cfbf2937e78c
Chirag Jadhwani <32898427+cjwonders@users.noreply.github.com>
2021-11-09 16:00:10 -0800

    maybeyeesssss

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" href="{{ secure_asset('css/AdminLTE.min.css') }}">
-x-app-layout>
+
+<x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
