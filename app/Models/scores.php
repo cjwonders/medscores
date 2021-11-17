efa766713df2ccac9d84e74b91b56efd54e63fbb
Chirag Jadhwani <32898427+cjwonders@users.noreply.github.com>
2021-11-17 14:22:11 -0800

    debug-2
    month

--- app/Models/scores.php
+++ app/Models/scores.php
@@ -10,7 +10,7 @@ class scores extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['question1', 'question2', 'question3', 'question4', 'question5', 'question6', 'question7', 'question8', 'question9', 'question10',
+    protected $fillable = ['month', 'question1', 'question2', 'question3', 'question4', 'question5', 'question6', 'question7', 'question8', 'question9', 'question10',
                             'question11', 'question12', 'question13', 'question14', 'question15', 'question16', 'question17', 'question18', 'question19', 'question20',
                             'question21', 'question22', 'question23'];
 
