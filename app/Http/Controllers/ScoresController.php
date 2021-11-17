a8ae06591e310092c833c7ea4a9989191c01e2f6
Chirag Jadhwani <32898427+cjwonders@users.noreply.github.com>
2021-11-17 14:08:31 -0800

    POST
    API store and get function should work now

--- app/Http/Controllers/ScoresController.php
+++ app/Http/Controllers/ScoresController.php
@@ -149,12 +149,29 @@ class ScoresController extends Controller
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'responses1' => 'nullable',
-            'responses2' => 'nullable',
-            'responses3' => 'nullable',
-            'responses4' => 'nullable',
-            'responses5' => 'nullable',
-            'student_alias' => 'required',
+            'question1' => 'required',
+            'question2' => 'required', 
+            'question3' => 'required', 
+            'question4' => 'required', 
+            'question5' => 'required',
+            'question6' => 'required',
+            'question7' => 'required',
+            'question8' => 'required',
+            'question9' => 'required',
+            'question10' => 'required',
+            'question11' => 'required', 
+            'question12' => 'required', 
+            'question13' => 'required',
+            'question14' => 'required',
+            'question15' => 'required', 
+            'question16' => 'required', 
+            'question17' => 'required', 
+            'question18' => 'required', 
+            'question19' => 'required', 
+            'question20' => 'required',
+            'question21' => 'required', 
+            'question22' => 'required', 
+            'question23' => 'required',
         ]);
 
         $validator->validate();
@@ -165,25 +182,33 @@ class ScoresController extends Controller
 
         $score_obj = new scores;
 
-        $score_obj->responses1 = $request->responses1;
-        $score_obj->responses2 = $request->responses2;
-        $score_obj->responses3 = $request->responses3;
-        $score_obj->responses4 = $request->responses4;
-        $score_obj->responses5 = $request->responses5;
-        
-        if (student::where('student_alias', $request->student_alias)->exists()) {
-            $student = student::where('student_alias', $request->student_alias)->first();
-            $score_obj->attachStudent($student->id);
-        } else {
-            $student = new student;
-            $student->student_alias = Crypt::encryptString($request->student_alias);
-            $student->save();
-            $score_obj->attachStudent($student->id);
-        }
+        $score_obj->question1 = $request->question1;
+        $score_obj->question2 = $request->question2;
+        $score_obj->question3 = $request->question3;
+        $score_obj->question4 = $request->question4;
+        $score_obj->question5 = $request->question5;
+        $score_obj->question6 = $request->question6;
+        $score_obj->question7 = $request->question7;
+        $score_obj->question8 = $request->question8;
+        $score_obj->question9 = $request->question9;
+        $score_obj->question10 = $request->question10;
+        $score_obj->question11 = $request->question11;
+        $score_obj->question12 = $request->question12;
+        $score_obj->question13 = $request->question13;
+        $score_obj->question14 = $request->question14;
+        $score_obj->question15 = $request->question15;
+        $score_obj->question16 = $request->question16;
+        $score_obj->question17 = $request->question17;
+        $score_obj->question18 = $request->question18;
+        $score_obj->question19 = $request->question19;
+        $score_obj->question20 = $request->question20;
+        $score_obj->question21 = $request->question21;
+        $score_obj->question22 = $request->question22;
+        $score_obj->question23 = $request->question23;
 
         $score_obj->save();
 
-        return responses(new ScoresResource($score_obj), 201);
+        return responses(new ScoresResource($score_obj), 221);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/scores', [ScoresController::class, 'indexScores']);
-Route::post('/scores', [ScoresController::class, 'indexScores']);
+Route::post('/scores', [ScoresController::class, 'store']);
