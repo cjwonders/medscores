4a551032e76257f1939acdd3418d288f5fce84c7
Chirag Jadhwani <32898427+cjwonders@users.noreply.github.com>
2021-11-18 15:36:09 -0800

    resource controller and routes
    API controller

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('scores', APIManager::class);
+Route::apiResource('scores', ScoresController::class);
