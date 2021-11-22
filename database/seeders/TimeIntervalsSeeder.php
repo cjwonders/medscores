cd1c9d821be76b9de316f1a143a325d7769f4103
Chirag Jadhwani <chirag@chiragjadhwani.com>
2021-11-22 13:58:54 -0800

    seeder fix

---
+++ database/seeders/TimeIntervalsSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use DB;
+
+class TimeIntervalsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $i = 1; 
+        while ($i <= 110){
+            DB::table('timestamps')->insert([
+                'test_id' => $i,
+                'response1' => rand(180,200),
+                'response2' => rand(290,300),
+                'response3' => rand(180,200),
+                'response4' => rand(450,500),
+                'response5' => rand(180,200),
+                'response6' => rand(90,100),
+                'response7' => rand(90,100),
+                'response8' => rand(90,100),
+                'response9' => rand(90,100),
+                'response10' => rand(90,100),
+                'response11' => rand(90,100),
+                'response12' => rand(90,100),
+                'response13' => rand(90,100),
+                'response14' => rand(90,100),
+                'response15' => rand(90,100),
+                'response16' => rand(90,100),
+                'response17' => rand(90,100),
+                'response18' => rand(90,100),
+                'response19' => rand(480,500),
+                'response20' => rand(90,100),
+                'response21' => rand(180,200),
+                'response22' => rand(90,100),
+                'response23' => rand(90,100),
+            ]);
+            $i++;
+    }
+}
+}
