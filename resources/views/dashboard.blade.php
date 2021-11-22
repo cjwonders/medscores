2e291bddf95e88e15f6e5c4413457d67bdce712f
Chirag Jadhwani <chirag@chiragjadhwani.com>
2021-11-22 15:21:25 -0800

    testing

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,20 +7,20 @@
     </x-slot>
 
     <div style="width: 80%;margin: 100px auto;">
-        {!! $chart->container() !!}
-    </div>
-    
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js" charset="utf-8">
-        {!! $chart->script() !!}
-    </script>
-
-    <div style="width: 80%;margin: 100px auto;">
         {!! $intervalschart->container() !!}
     </div>
     
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js" charset="utf-8">
-        {!! $intervalschart->script() !!}
     </script>
+    {!! $intervalschart->script() !!}
+
+    <div style="width: 80%;margin: 100px auto;">
+        {!! $chart->container() !!}
+    </div>
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js" charset="utf-8">
+    </script>
+    {!! $chart->script() !!}
 
     <div class="py-12">
         <div class="max-w-8xl mx-auto sm:px-6 lg:px-8">
