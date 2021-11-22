bf7ddf3aff0fd014aec35eeef54d09ccb1548640
Chirag Jadhwani <chirag@chiragjadhwani.com>
2021-11-22 15:16:15 -0800

    testing

--- app/Http/Controllers/ScoresController.php
+++ app/Http/Controllers/ScoresController.php
@@ -44,7 +44,7 @@ class ScoresController extends Controller
         $intervalschart->labels(['question1', 'question2', 'question3', 'question4', 'question5', 'question6', 'question7', 'question8', 'question9', 'question10',
         'question11', 'question12', 'question13', 'question14', 'question15', 'question16', 'question17', 'question18', 'question19', 'question20',
         'question21', 'question22', 'question23']);
-        $intervalschart->dataset('Time Interval by Question', 'bar', $this->TimeIntervalAverage())->options([
+        $intervalschart->dataset('Time Interval by Question', 'line', $this->TimeIntervalAverage())->options([
             'fill' => 'true',
             'borderColor' => '#51C1C0'
         ]);
