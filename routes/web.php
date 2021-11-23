a85574025086f6932e8637ca884c6f9f06284e7f
Chirag Jadhwani <chirag@chiragjadhwani.com>
2021-11-23 10:27:46 -0800

    testing

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'ScoresController@index')->middleware(['auth']);
 
+Route::get('/dashboard', 'ScoresController@index')->middleware(['auth']);
+
 require __DIR__.'/auth.php';
